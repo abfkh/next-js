534934585445571e24684313d92855a8ffeb11cb
abfkh <84881348+abfkh@users.noreply.github.com>
2023-06-09 03:49:59 +0530

    update

--- src/app/portfolio/[category]/page.jsx
+++ src/app/portfolio/[category]/page.jsx
@@ -1,7 +1,13 @@
 import React from "react";
+import styles from "./page.module.css";
 
-const Category = () => {
-  return <div>Category</div>;
+const Category = ({ params }) => {
+  console.log(params);
+  return (
+    <div className={styles.container}>
+      <h1>{params.category}</h1>
+    </div>
+  );
 };
 
 export default Category;
--- src/app/portfolio/page.jsx
+++ src/app/portfolio/page.jsx
@@ -1,38 +1,51 @@
 import React from "react";
 import styles from "./page.module.css";
 import Image from "next/image";
+import Link from "next/link";
 const Portfolio = () => {
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>Our Works</h1>
       <div className={styles.gallery}>
         <div className={styles.galleryContainer}>
-          <Image
-            Src="https://images.unsplash.com/photo-1542831371-29b0f74f9713?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80"
-            className={styles.img}
-          />
-          <h2 className={styles.h1}>Carify app - UX/UI Case Study</h2>
+          <Link href="portfolio/ux">
+            <Image
+              Src="https://images.unsplash.com/photo-1542831371-29b0f74f9713?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80"
+              className={styles.img}
+            />
+
+            <h2 className={styles.h1}>Carify app - UX/UI Case Study</h2>
+          </Link>
         </div>
         <div className={styles.galleryContainer}>
-          <Image
-            Src="https://images.unsplash.com/photo-1542831371-29b0f74f9713?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80"
-            className={styles.img}
-          />
-          <h2 className={styles.h1}>Carify app - UX/UI Case Study</h2>
+          <Link href="portfolio/ux">
+            <Image
+              Src="https://images.unsplash.com/photo-1542831371-29b0f74f9713?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80"
+              className={styles.img}
+            />
+
+            <h2 className={styles.h1}>Carify app - UX/UI Case Study</h2>
+          </Link>
         </div>
         <div className={styles.galleryContainer}>
-          <Image
-            Src="https://images.unsplash.com/photo-1542831371-29b0f74f9713?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80"
-            className={styles.img}
-          />
-          <h2 className={styles.h1}>Carify app - UX/UI Case Study</h2>
+          <Link href="portfolio/backend">
+            <Image
+              Src="https://images.unsplash.com/photo-1542831371-29b0f74f9713?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80"
+              className={styles.img}
+            />
+
+            <h2 className={styles.h1}>Carify app - UX/UI Case Study</h2>
+          </Link>
         </div>
         <div className={styles.galleryContainer}>
-          <Image
-            Src="https://images.unsplash.com/photo-1542831371-29b0f74f9713?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80"
-            className={styles.img}
-          />
-          <h2 className={styles.h1}>Carify app - UX/UI Case Study</h2>
+          <Link href="portfolio/ui">
+            <Image
+              Src="https://images.unsplash.com/photo-1542831371-29b0f74f9713?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80"
+              className={styles.img}
+            />
+
+            <h2 className={styles.h1}>Carify app - UX/UI Case Study</h2>
+          </Link>
         </div>
       </div>
     </div>
