95c56e941072631015c6e168150bd29495cc1969
abfkh <84881348+abfkh@users.noreply.github.com>
2023-06-09 23:08:21 +0530

    change color of title

--- src/app/portfolio/layout.jsx
+++ src/app/portfolio/layout.jsx
@@ -3,8 +3,8 @@ import styles from "./page.module.css";
 
 const Layout = ({ children }) => {
   return (
-    <div>
-      <h1 className={styles.mainTitle}>My work</h1>
+    <div className={styles.container}>
+      <h1 className={styles.titles}>My work</h1>
       {children}
     </div>
   );
